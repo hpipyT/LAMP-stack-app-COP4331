abcbebcee1c875434e6086a3cde00d9e6e0f24d6
egallardop13 <54786163+egallardop13@users.noreply.github.com>
2022-06-04 23:01:31 -0400

    Add files via upload

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,28 +1,35 @@
 <?php
 	$exist = True;
-	
+
 	$inData = getRequestInfo();
 	#$color = $inData["color"];
 	$FirstName = $inData["FirstName"];
 	$LastName = $inData["LastName"];
 	$Login = $inData["Login"];
 	$Password = $inData["Password"];
+	$CheckUser = "SELECT * from Users WHERE Login = '$Login'";
+
 
 	#$conn = new mysqli("localhost", "userId", "password", "database");
 	#$conn = new mysqli("localhost", "NewUser", "NewPass", "TestFirst", "TestLast", "COP4331");
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
+		$CheckQuery = mysqli_query($conn,$CheckUser);
+		if(mysqli_num_rows($CheckQuery)>0){
+			echo "Username taken";
+		}else{
+
 		$stmt = $conn->prepare("INSERT into Users (Login, FirstName, LastName, Password) VALUES(?,?,?,?)");
 		//$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		//$stmt->bind_param("ssss", $inData["userId"], $inData["firstName"], $inData["lastName"], $inData["password"]);
 		$stmt->bind_param("ssss", $Login, $FirstName, $LastName, $Password);
 		$stmt->execute();
-		
+
 		// if( $row = $result->fetch_assoc()  )
 		// {
 		// 	echo "Username already taken<br>";
@@ -32,6 +39,7 @@
 		$conn->close();
 		print "Account Registered!";
 		//returnWithError("");
+   }
 	}
 
 	function getRequestInfo()
@@ -44,11 +52,11 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
